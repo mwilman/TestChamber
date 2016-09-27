95c7502c8000684f3416dfa3bbda1e09860704eb
mwilman <mwilman@brockhaus-ag.de>
2016-09-27 16:19:55 +0200

    3 Zeilen

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Zweite Änderung!");
+            Console.WriteLine("Noch eine Änderung");
             Console.ReadKey();
         }
     }
